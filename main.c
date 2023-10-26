17acf1b1dd5372fb8e21f42fa8f84303c4e393f7
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2023-10-26 17:45:22 +0200

    Adding writeN and readN

--- main.c
+++ main.c
@@ -12,6 +12,38 @@
 #define check(status) if(status) {perror("Error"); exit(1);}
 
 
+ssize_t writeN(int fd, void *ptr, size_t n) {  
+   size_t   nleft;
+   ssize_t  nwritten;
+ 
+   nleft = n;
+   while (nleft > 0) {
+     if((nwritten = write(fd, ptr, nleft)) < 0) {
+        if (nleft == n) return -1;
+        else break;
+     } else if (nwritten == 0) break; 
+     nleft -= nwritten;
+     ptr   += nwritten;
+   }
+   return(n - nleft);
+}
+
+ssize_t readN(int fd, void *ptr, size_t n) {  
+   size_t   nleft;
+   ssize_t  nread;
+ 
+   nleft = n;
+   while (nleft > 0) {
+     if((nread = read(fd, ptr, nleft)) < 0) {
+        if (nleft == n) return -1;
+        else break;
+     } else if (nread == 0) break;
+     nleft -= nread;
+     ptr   += nread;
+   }
+   return(n - nleft);
+}
+
 int main(void) {
     int server_socket;
     check((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0);
@@ -31,19 +63,23 @@ int main(void) {
     socklen_t addr_size;
     int client_socket;
     struct sockaddr_in client_address;
-    char buffer[] = "HTTP/1.0 200 OK\r\n"
+    char response[] = "HTTP/1.0 200 OK\r\n"
                     "Content-Type: text/html\r\n\r\n"
                     "<h>Hello from server!</h>\r\n";
 
-    printf("%s", buffer);
+    char buffer[BUFFER_SIZE];
+
 
     while(1){
         addr_size = sizeof(client_address);
         client_socket = accept(server_socket, (struct sockaddr*)&client_address, &addr_size);
         fprintf(stderr, "[+]Client connected.\n");
 
-        write(client_socket, buffer, strlen(buffer));
+        read(client_socket, buffer, sizeof(buffer));
+        fprintf(stderr, "[+]%s", buffer);
 
+        writeN(client_socket, buffer, strlen(response));
+      
         close(client_socket);
         fprintf(stderr, "[+]Client disconneted.\n");
 
